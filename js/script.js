27e732248ee62b8f3c58e738ea4004daae19a1e7
Joyce <riberjoy@outlook.com>
2019-12-20 16:49:50 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
-
 var vivo = true;
-var direita = 0;
 
-$(document).ready(function(){
-    $("#play").click(function(){
+var posicaoAtual = parseInt($("#cobra").offset().left);
+
+var xLimite = 800;
+var xLimite = 600;
+
+
+
+var velocidade = 1000;
+
+$(document).ready(function () {
+    $("#play").click(function () {
         $("#pontos").css("display", "flex");
         $("#play").css("display", "none");
         move();
     })
 })
 
-function move(){
-    var dimensao = setInterval(movimento, 100);
+function move() {
+    var dimensao = setInterval(movimento, velocidade);
     function movimento() {
-        if (direita == 800) {
-        //   direita = 0;
-        //   $("#cobra").css("margin-left", direita+"px");
-        $("#cobra").css("display", "none");
-        dimensao = clearInterval();
-        } else { 
-            direita = direita+20;
-            $("#cobra").css("margin-left", direita+"px");
+        if (xLimite == 0) {
+            //   direita = 0;
+            //   $("#cobra").css("margin-left", direita+"px");
+            $("#cobra").css("display", "none");
+            dimensao = clearInterval();
+        } else {
+            xLimite = xLimite -1;
+            $(".posicao").css("left",(posicaoAtual+1)+"px");
+            $("#pontos").html($("#cobra").offset().left);
+            // document.getElementById("#pontos").innerHTML = esquerda ;
         }
     }
 }
 
+// Para adicionar uma nova imaem (mais uma parte da cobrinha)
+// usar a seguinte estrutura: 
+// $(document).ready(function () {
+//     // caso passar pela mesma posição da maça
+//     function cresceCobra() {
+//         var imagem = $("<img/>", { src: "imagens/corpo_Cobrinha.svg"});
+//         $("section div").append(imagem);
+//     }
+// })
